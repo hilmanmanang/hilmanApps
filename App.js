ef10ae70224abf9e6f9fc3f0299278b0f9607812
Muhammad Hilman Bin Ahmad <hilmanmanang@gmail.com>
2021-07-04 10:16:08 +0800

    react native button

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Platform, StyleSheet, Text, View, Image, Alert, Button, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
+  const handlePress = function hello() {
+    console.log('hello')
+  }
+
+  const pressAlert = function alrt() {
+    Alert.alert("My Title", "My Body", [
+      { text: "Yes", onPress: () => console.log('Yes Clicked') },
+      { text: "No", onPress: () => console.log('No Clicked') },
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/favicon.png')}/>
+      <Text style={styles.text} onPress={handlePress}>hello there</Text>
+      <TouchableWithoutFeedback>
+        <Image source={{
+          width: 200,
+          height: 200,
+          uri: 'https://picsum.photos/200'
+        }}/>
+      </TouchableWithoutFeedback>
+      <Button
+        title="Click Me"
+        onPress={pressAlert}
+      />
     </View>
   );
 }
@@ -15,7 +36,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+  text: {
+    fontSize: 40,
+    color: 'black',
+    fontWeight: '800',
+  }
 });
